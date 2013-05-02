49b25e817969382c70cbc1bdd47e9f8bf8e181ee
houstonn <houstonn@gmail.com>
2013-05-01 22:41:28 -0400

    touchup

--- products/m7.mk
+++ products/m7.mk
@@ -21,8 +21,19 @@ PRODUCT_MODEL := HTC One
 PRODUCT_MANUFACTURER := HTC
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=occam BUILD_FINGERPRINT=google/bm/m7:4.2.2/JDQ39/573038:user/release-keys PRIVATE_BUILD_DESC="m7-user 4.2.2 JDQ39 573038 release-keys" BUILD_NUMBER=573038
+PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=htc_m7 BUILD_FINGERPRINT=htc/bm/m7:4.2.2/JDQ39/573038:user/release-keys PRIVATE_BUILD_DESC="m7-user 4.2.2 JDQ39 573038 release-keys" BUILD_NUMBER=573038
 
 # Copy Bootanimation
 PRODUCT_COPY_FILES += \
-    vendor/htc/m7/prebuilt/common/media/test-3-ani.zip:system/media/bootanimation.zip
+    vendor/rootbox/prebuilt/common/bootanimation/test-3-ani.zip:system/media/bootanimation.zip
+
+PRODUCT_COPY_FILES += \
+    vendor/rootbox/prebuilt/common/app/com.miuimusic232.apk:system/app/com.miuimusic232.apk
+
+PRODUCT_COPY_FILES += \
+    vendor/rootbox/prebuilt/common/media/sounds/xperia.ogg:system/media/audio/ringtones/xperia.ogg \
+    vendor/rootbox/prebuilt/common/media/sounds/xperia_z_breeze.ogg:system/media/audio/ringtones/xperia_z_breeze.ogg \
+    vendor/rootbox/prebuilt/common/media/sounds/xperia_z_rise.ogg:system/media/audio/ringtones/xperia_z_rise.ogg \
+    vendor/rootbox/prebuilt/common/media/sounds/xperia1_z.ogg:system/media/audio/ringtones/xperia1_z.ogg \
+    vendor/rootbox/prebuilt/common/media/sounds/xperia2_z.ogg:system/media/audio/ringtones/xperia2_z.ogg \
+    vendor/rootbox/prebuilt/common/media/sounds/Whistle.ogg:system/media/audio/notifications/s_whistle.ogg
